1367aec6521d8fd6c00242e052119481232440d0
jaume <jaume@owner.home>
2020-05-21 13:52:31 +0200

    Prueba

---
+++ src/main/java/com/mycompany/actividadgithub/Ejercicio.java
@@ -0,0 +1,12 @@
+package com.mycompany.actividadgithub;
+
+/**
+ *
+ * @author jaume
+ */
+public class Ejercicio {
+    public static void main(String[] args) {
+        System.out.println("Esto es una ejercicio de Rafa");
+    }
+    
+}
